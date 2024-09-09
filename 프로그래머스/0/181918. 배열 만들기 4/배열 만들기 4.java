c5ea80cc82d7c97a43a8aca6059d56f578fed2a6
programmershine <151138652+programmershine@users.noreply.github.com>
2024-09-09 20:46:45 +0900

    [level 0] Title: 배열 만들기 4, Time: 21.20 ms, Memory: 87.4 MB -BaekjoonHub

--- 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.java
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.java
@@ -2,16 +2,36 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] arr) {
-        ArrayList<Integer> list = new ArrayList<>();
+                
+        int[] stk = {};
+        ArrayList<Integer> list = new ArrayList<>();        
         
-        for (int i = 0; i < arr.length; i++) {
+        for(int i = 0; i < arr.length; i++){
+           
+            //if(list.size() == 0){
+            //    list.add(arr[i]);
+            //    i++;
+            //}else{
+            //    if(list.get(list.size()-1) < arr[i]){
+            //        list.add(arr[i]);
+            //        i++;
+            //   }else{
+            //        list.remove(list.size()-1);
+            //    }
+            //}
+
+            // if문 while문의 차이를 알아내야함
+            
             while (!list.isEmpty() && list.get(list.size() - 1) >= arr[i]) {
                 list.remove(list.size() - 1);
             }
             list.add(arr[i]);
-        }
         
-        // ArrayList를 int[]로 변환
-        return list.stream().mapToInt(Integer::intValue).toArray();
+            
+        }
+        stk = list.stream().mapToInt(Integer::intValue).toArray();
+        
+        
+        return stk;
     }
 }
