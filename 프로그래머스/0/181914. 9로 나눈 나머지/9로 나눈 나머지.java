e0ee659753cd947b2c777e2481e00face06b99ec
programmershine <151138652+programmershine@users.noreply.github.com>
2024-09-10 14:38:18 +0900

    [level 0] Title: 9로 나눈 나머지, Time: 195.89 ms, Memory: 112 MB -BaekjoonHub

--- 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.java
+++ 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.java
@@ -4,7 +4,7 @@ class Solution {
         
         // 문자열을 한 자리씩 순회
         for (int i = 0; i < number.length(); i++) {
-            // 현재 자리의 숫자 추출
+            // 현재 자리의 숫자 추출 *새로알아간다 getNumericValue 하면 char를 숫자로 바꿔줌 인티저파스인트랑 비슷
             int digit = Character.getNumericValue(number.charAt(i));
             
             // 현재 자리수를 포함하여 나머지를 갱신
