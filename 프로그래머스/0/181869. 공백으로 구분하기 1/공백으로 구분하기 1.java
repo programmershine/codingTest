17c37054d117ad68296545dd2c489a6475a8ff32
programmershine <151138652+programmershine@users.noreply.github.com>
2024-09-18 01:21:12 +0900

    [level 0] Title: 공백으로 구분하기 1, Time: 0.24 ms, Memory: 73.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181869. 공백으로 구분하기 1/공백으로 구분하기 1.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String[] solution(String my_string) {
+        String[] answer = {};
+        
+        String[] result = my_string.split(" ");
+        
+        answer = result.clone();
+        
+        return answer;
+    }
+}
